69d77206a592a24efd2f15cd7f6ff341017457df
Ochiengsteven <stevenochieng6305@gmail.com>
2024-08-16 13:02:03 +0300

    ADD ROUTER

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./redux/reducers/counterSlice";
 
 const App = () => {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
   return (
     <div>
-      <p>app</p>
+      <p className="text-red-500">app</p>
+      <div>
+        <p>Count: {count}</p>
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
+      </div>
     </div>
   );
 };
---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+// app/store.js
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./reducers/counterSlice";
+
+const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
+
+export default store;
